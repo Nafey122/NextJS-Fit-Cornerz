07c7591d2277b013fea5ba7a04d611d6712db79e
Nafey122 <88371480+Nafey122@users.noreply.github.com>
2022-09-24 13:17:32 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -50,20 +50,18 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+      <div className="">
+        <ResponsiveHeaderJsx />
+        <HeaderJsx />
+        <HeroSectionJsx />
+        <CategoryJsx />
+        <BlogsJsx />
+        <BMIJsx />
+        <GameJsx />
+        <PlayingKidJsx />
+        <DrinkJsx />
+        <FitnessJsx />
+        <MobileAppJsx />
+      </div>
   )
 }
